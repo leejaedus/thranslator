da988a8e10eb6f61b44054026b24bec1373ee745
leejaedus <leejaeduss@gmail.com>
2016-10-26 01:17:08 +0900

    Add dev directory

--- dev/server.development.js
+++ dev/server.development.js
@@ -2,7 +2,7 @@ import express from 'express';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from './webpack.config.development';
+import config from '../config/webpack.config.development';
 
 const app = express();
 const compiler = webpack(config);

893149c081fe6d36f0787401752270659f65b580
leejaedus <leejaeduss@gmail.com>
2016-10-28 00:14:04 +0900

    Fix broken link

--- dev/main.development.js
+++ dev/main.development.js
@@ -10,7 +10,7 @@ function createWindow () {
   // 새로운 브라우저 창을 생성합니다.
   win = new BrowserWindow({ width: 800, height: 600 });
   // 그리고 현재 디렉터리의 index.html을 로드합니다.
-  win.loadURL(`file://${__dirname}/src/index.html`);
+  win.loadURL(`file://${__dirname}/../src/index.html`);
   // 개발자 도구를 엽니다.
   win.webContents.openDevTools();
   // 창이 닫히면 호출됩니다.

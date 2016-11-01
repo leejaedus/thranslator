aee4ea3d648f69334e664dbad5d2a768d4002b40
leejaedus <leejaeduss@gmail.com>
2016-11-01 23:31:11 +0900

    Set github client object to global state

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,15 +1,21 @@
 /* @flow */
 import * as LoginActions from '../actions/LoginActions';
+import github from 'octonode';
 
 const initialState = {
   token: window.localStorage.getItem('githubtoken') || '',
+  client: window.localStorage.getItem('githubtoken') ?
+    github.client(window.localStorage.getItem('githubtoken')) : null,
 };
 
 export default function login(state: Object = initialState, action: Object): Object {
   switch (action.type) {
     case LoginActions.LOGIN:
       window.localStorage.setItem('githubtoken', action.token);
-      return state;
+      return {
+        token: action.token,
+        client: github.client(action.token),
+      };
     default:
       return state;
   }
